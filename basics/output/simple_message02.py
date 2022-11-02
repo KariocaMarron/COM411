d23a35c120e315bd33c12cca2a2281c2c1ae9c87
KariocaMarron <5bataj10@solent.ac.uk>
2022-11-02 21:24:05 +0000

    Added code to display a simple message.

---
+++ basics/output/simple_message02.py
@@ -0,0 +1,7 @@
+# Display message to the standard output
+print("System Failure Imminent!")
+
+print() # Displays a blank line. Comments can be placed on the same line.
+
+# Displays "some text" to the standard output. It is preferable to place comments before the code.
+print("some text")
