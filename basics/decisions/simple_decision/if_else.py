05f61d4a3689c862b4b3593047ee8b23d045405f
KariocaMarron <5bataj10@solent.ac.uk>
2022-11-04 20:36:16 +0000

    "Added conditional code and final message"

--- basics/decisions/simple_decision/if_else.py
+++ basics/decisions/simple_decision/if_else.py
@@ -3,5 +3,12 @@ print("Please enter the activity to be performed.")
 activity = input()
 
 
+# Determine if the activity is calculate
+if activity == "calculate":
+    print("Performing calculations...")
+else:
+    print("Performing activity...")
 
+# Display message
+print("Activity completed.")
 
