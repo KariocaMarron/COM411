fcf55eb3ee63af0cf1772d8721864b2b7f41fe4b
KariocaMarron <5bataj10@solent.ac.uk>
2022-11-04 21:34:13 +0000

    Display relevant message

--- basics/decisions/simple_decision/modolo_operator.py
+++ basics/decisions/simple_decision/modolo_operator.py
@@ -2,3 +2,8 @@
 print("Please enter a whole number.")
 number = int(input())
 
+# Display relevant message
+if number % 2 == 0:
+    print(f"The number {number} is an even number.")
+else:
+    print(f"The number {number} is an odd number.")
