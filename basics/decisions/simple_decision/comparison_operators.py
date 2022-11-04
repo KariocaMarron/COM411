82a1db67aeff5d0c07a9443419eb852cf1dd216c
KariocaMarron <5bataj10@solent.ac.uk>
2022-11-04 21:37:27 +0000

    Determine which message to display

--- basics/decisions/simple_decision/comparison_operators.py
+++ basics/decisions/simple_decision/comparison_operators.py
@@ -4,3 +4,12 @@ first_number = int(input())
 
 print("Please enter the second number.")
 second_number = int(input())
+
+
+# Determine which message to display
+if first_number < second_number:
+    print("The first number is the smallest.")
+elif first_number > second_number:
+    print("The second number is the smallest.")
+else:
+    print("The two numbers are equal.")
