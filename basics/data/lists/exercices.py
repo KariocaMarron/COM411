66c73c8eae72bdcf29b6cbc0c489da42403c7e8a
KariocaMarron <5bataj10@solent.ac.uk>
2022-11-09 10:30:11 +0000

    working with cwd list

--- basics/data/lists/exercices.py
+++ basics/data/lists/exercices.py
@@ -1,4 +1,4 @@
-new_lis = [2, 8, 1, 4, 6]
+my_lis = [6, 1, 9, 2]
 
-for x, new_val in enumerate(new_lis):
-	print (x, ",",new_val)
+for new_indic in range(len(my_lis)):
+	print(new_indic, my_lis[new_indic])
