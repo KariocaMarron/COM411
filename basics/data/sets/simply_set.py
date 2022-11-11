5980deb2e516ab0cd5f438e4d7067527fd90c583
KariocaMarron <5bataj10@solent.ac.uk>
2022-11-11 10:12:16 +0000

    first dictionary

---
+++ basics/data/sets/simply_set.py
@@ -0,0 +1,6 @@
+observed = set() # Creating the variable named "observed" and given a value of "set()"
+observations = {"Flying car", "Sky Scraper", "Laser", "Dome"} # Creating a variable "observations" and attributting them the list
+
+
+print(set(observations)) # Giving instructions to print the
+
