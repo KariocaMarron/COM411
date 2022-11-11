dc4e8745c3a3c3d099e90d4c9641b559aa837685
KariocaMarron <5bataj10@solent.ac.uk>
2022-11-11 10:11:00 +0000

    first dictionary

---
+++ WEEK 6/set.py
@@ -0,0 +1,8 @@
+lectures = set() # lists uses index - set is ash memory - no order = You cannot duplicate values with sets
+
+lectures = ("jose", "Carlos")
+lectures = (["Batalha", "De", "Vasconcelos"])
+
+for lec in lectures:
+    print(lec)
+# set do not maintain the order and do not allow duplicates
--- basics/data/lists/exercices.py
+++ basics/data/lists/exercices.py
@@ -5,10 +5,4 @@ import os
 
 # Get the list of all files and directories
 # in the root directory
-path = "/"
-dir_list = os.listdir(path)
-
-print("Files and directories in '", path, "' :")
-
-# print the list
-print(dir_list)
+print(f"Current Working Directory: ", os.getcwd())
