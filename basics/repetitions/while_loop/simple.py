5e366eb9cc73c6f89e3e5e842a9220a494a0cc81
KariocaMarron <5bataj10@solent.ac.uk>
2022-12-14 17:11:13 +0000

    Added code to demostarte the use of a simple while loop

---
+++ basics/repetitions/while_loop/simple.py
@@ -0,0 +1,18 @@
+# Ask how many cables to remove
+print("How many cables should I remove?")
+
+# Defining variable iterations at initial value 0
+iterations = 0
+
+# Define variable amount_cab
+amount_cab = int(input())
+
+print(f"\t\tStart removing ** {amount_cab} ** cables\n")
+
+# Count how many cables user input
+while iterations < amount_cab:
+
+    iterations = iterations + 1
+    print("Cable removed\n")
+
+print(f"All the **_ {amount_cab} _** cables were removed as requested!")
